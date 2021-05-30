a7ac9adb255827fe3e48bf876c55407795f7f9da
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2021-05-30 19:11:12 +0300

    ref styles

--- media/src/main.js
+++ media/src/main.js
@@ -2,10 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import globalComponents from "./utils/global-components"
 import vscode from "./plugins/vscode";
+import RuleEngineEditor from "@ruleenginejs/ruleengine-editor-vue";
+import "vscode-codicons/dist/codicon.css";
 
 const app = createApp(App);
 
 app.use(globalComponents);
 app.use(vscode);
+app.use(RuleEngineEditor);
 
 app.mount('#app');
