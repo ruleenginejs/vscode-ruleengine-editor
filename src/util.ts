031bb1b4c95757a087fa11a8bc16ffd2f181763d
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2021-06-22 02:34:02 +0300

    fix detail

--- src/util.ts
+++ src/util.ts
@@ -12,33 +12,33 @@ const stepQuickPickItems: Array<vscode.QuickPickItem> = [
   {
     label: StepType.start,
     description: "navigation",
-    detail: "From this step the rule will run. Only one for the rule."
+    detail: "This step indicates start of rule. Only one for rule."
   },
   {
     label: StepType.end,
     description: "navigation",
-    detail: "Indicates the end of the rule. Everyone port must end with this step."
+    detail: "This step indicates end of rule. Everyone port must end with this step."
   },
   {
     label: StepType.error,
     description: "navigation",
-    detail: "If any error occurs in handlers this step is run. Only one for the rule."
+    detail: "This step run when error occurs in handlers. Only one for rule."
   },
   {
     label: StepType.single,
     description: "handler",
-    detail: "This step run the handler if specified."
+    detail: "This step run handler if specified."
   },
   {
     label: StepType.composite,
     description: "handler",
-    detail: "This step may contain other steps."
+    detail: "This step contains other steps."
   }
 ];
 
 export async function showStepQuickPick(): Promise<StepType | undefined> {
   const result = await vscode.window.showQuickPick(stepQuickPickItems, {
-    placeHolder: 'Please select the type of step'
+    placeHolder: 'Select Step'
   });
   if (!result) {
     return undefined;
