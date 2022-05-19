371c4a67360cd0131884105eb4187f26190a789f
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2022-05-19 12:41:33 +0300

    chore: format

--- media/editor/tailwind.config.js
+++ media/editor/tailwind.config.js
@@ -1,25 +1,23 @@
 module.exports = {
-  content: [
-    './views/**/*.vue'
-  ],
+  content: ['./views/**/*.vue'],
   theme: {
     extend: {
       colors: {
-        "text-link-foreground": "var(--vscode-textLink-foreground)",
-        "editor-hint-foreground": "var(--editor-hint-foreground)"
+        'text-link-foreground': 'var(--vscode-textLink-foreground)',
+        'editor-hint-foreground': 'var(--editor-hint-foreground)'
       },
       spacing: {
-        "5sp": "0.3125rem",
-        "10sp": "0.625rem",
-        "13sp": "0.8125rem"
+        '5sp': '0.3125rem',
+        '10sp': '0.625rem',
+        '13sp': '0.8125rem'
       },
       fontSize: {
-        "13sp": ['0.8125rem', { lineHeight: '1.1375rem' }]
+        '13sp': ['0.8125rem', { lineHeight: '1.1375rem' }]
       },
       fontFamily: {
-        vscode: "var(--vscode-font-family)"
+        vscode: 'var(--vscode-font-family)'
       }
     }
   },
   plugins: []
-}
+};
